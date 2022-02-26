34d6aadbc05436501eb0b581bee2712b0ad4a8b3
deji100 <adeyemideji9@gmail.com>
2022-02-26 22:05:05 +0100

    reconfigured settings.py file

--- Ecommerce/core/settings.py
+++ Ecommerce/core/settings.py
@@ -28,14 +28,16 @@ load_dotenv()
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-# DEBUG = False
+# DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = [
-    'lagos-today.herokuapp.com',
-    '127.0.0.1:8000',
-    '127.0.0.1'
-    ]
+# ALLOWED_HOSTS = [
+#     'lagos-today.herokuapp.com',
+#     '127.0.0.1:8000',
+#     '127.0.0.1'
+#     ]
+
+ALLOWED_HOSTS = [ '*' ]
 
 INTERNAL_IPS = [
     '127.0.0.1',

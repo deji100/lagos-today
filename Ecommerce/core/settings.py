66e8f8e275544c451018ed78f661517ad40d8e55
deji100 <adeyemideji9@gmail.com>
2022-02-27 01:25:52 +0100

    edited requirements.txt

--- Ecommerce/core/settings.py
+++ Ecommerce/core/settings.py
@@ -28,9 +28,9 @@ load_dotenv()
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-# DEBUG = False
-    
+# DEBUG = True
+DEBUG = False
+
 ALLOWED_HOSTS = [
     'lagos-today.herokuapp.com',
     '127.0.0.1:8000',

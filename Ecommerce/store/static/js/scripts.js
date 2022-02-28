5331c7d0baff046b3c30a92d63a7db9a9d59c109
deji100 <adeyemideji9@gmail.com>
2022-02-28 09:10:57 +0100

    Updated UI

--- Ecommerce/store/static/js/scripts.js
+++ Ecommerce/store/static/js/scripts.js
@@ -1,54 +1,54 @@
 var updateBtns = document.getElementsByClassName('update-cart');
       
-      for (var i = 0; i < updateBtns.length; i++) {
-          updateBtns[i].addEventListener('click', function() {
-              var productId = this.dataset.product;
-              var action = this.dataset.action;
-              console.log('productId:', productId, 'action:', action);
-      
-              console.log('USER:', user)
-      
-              if(user === "AnonymousUser") {
-                console.log('Not logged in.')
-              }else {
-                updateUserCart(productId, action)
-              }
-          })
-      }
+for (var i = 0; i < updateBtns.length; i++) {
+    updateBtns[i].addEventListener('click', function() {
+        var productId = this.dataset.product;
+        var action = this.dataset.action;
+        console.log('productId:', productId, 'action:', action);
 
-      function updateUserCart(product_id, action) {
-        console.log('User is authenticated, sending data...')
+        console.log('USER:', user)
 
-        var url = '/updateProduct/'
+        if(user === "AnonymousUser") {
+          console.log('Not logged in.')
+        }else {
+          updateUserCart(productId, action)
+        }
+    })
+}
 
-        fetch(url, {
-          method:'POST',
-          headers:{
-            'Content-Type':'application/json',
-            'X-CSRFToken': csrftoken,
-          },
-          body:JSON.stringify({'productId': product_id, 'action': action}) 
-        })
-        
-        .then((response) =>{
-          return response.json()
-        })
+function updateUserCart(product_id, action) {
+  console.log('User is authenticated, sending data...')
 
-        .then((data) =>{
-          console.log('data:', data)
-          location.reload()
-        })
+  var url = '/updateProduct/'
 
-        .catch( () =>{
-          console.log('Error')
-        })
-      }
-
-
-// function showSearch() {
-//   var search = document.getElementsByClassName('search');
-//   var icon  = document.getElementsByClassName('search-icon');
+  fetch(url, {
+    method:'POST',
+    headers:{
+      'Content-Type':'application/json',
+      'X-CSRFToken': csrftoken,
+    },
+    body:JSON.stringify({'productId': product_id, 'action': action}) 
+  })
   
-//   search.setAttribute('class', 'showSearch');
-//   icon.style.visibility = 'hidden';
-// }
+  .then((response) =>{
+    return response.json()
+  })
+
+  .then((data) =>{
+    console.log('data:', data)
+    location.reload()
+  })
+
+  .catch( () =>{
+    console.log('Error')
+  })
+}
+
+
+let toggle = document.querySelector('.toggle');
+let pro_nav = document.querySelector('#pro-nav')
+
+toggle.onclick = () => {
+    toggle.classList.toggle('active');
+    pro_nav.classList.toggle('active');
+}
